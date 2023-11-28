5c79fa11d760cae5874ab82e220a5022b51172ba
moon <gkaqhd0000@naver.com>
2023-11-28 23:47:24 +0900

    feat : DB connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { validate } from './validation/env.validation';
 import { AuthModule } from './auth/auth.module';
 import { AuthController } from './auth/auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `${process.env.NODE_ENV}.env`,
+      envFilePath: `config/${process.env.NODE_ENV}.env`,
       validate,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+       return {
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        database: configService.get('DATABASE_NAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false,
+       }
+      }
     }),
     AuthModule,
   ],
