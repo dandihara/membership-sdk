a13de7bd06938709358434a63142f613bbe36a66
moon <gkaqhd0000@naver.com>
2023-11-29 22:24:44 +0900

    fix : login type 변경

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,17 +1,20 @@
-import { IsString, IsEnum,IsDefined } from 'class-validator';
+import { IsString, IsEnum,IsDefined, IsOptional, IsEmail } from 'class-validator';
 import { LoginType } from './auth.enum';
 
 export class LoginRequest {
     @IsDefined()
+    @IsEmail()
+    email: string;
+
+    @IsDefined()
+    @IsString()
+    password: string;
+
+    @IsOptional()
     @IsEnum(LoginType)
-    type: LoginType;
-    
-    @IsString()
-    id?: string;
+    type?: LoginType;
 
-    @IsString()
-    password?: string;
-
+    @IsOptional()
     @IsString()
     token?: string;
 }
--- src/auth/dto/auth.enum.ts
+++ src/auth/dto/auth.enum.ts
@@ -1,5 +1,5 @@
+// 소셜 로그인만 관리.
 export enum LoginType {
     GOOGLE = 'google',
     KAKAO = 'kakao',
-    NORMAL = 'normal'
 }
